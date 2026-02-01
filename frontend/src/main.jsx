44482800591cbe6183d77be6e5f8a1157893bcf2
Koyyada-Akhil <akhilakkhi6@gmail.com>
2026-02-01 22:47:49 +0530

    main updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/Mern-Stack-Project/">
+  <BrowserRouter >
     <App />
   </BrowserRouter>
 );
