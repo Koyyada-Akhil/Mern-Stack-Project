f7591da518eac14d649f5060b09df6593755ad0c
Koyyada-Akhil <akhilakkhi6@gmail.com>
2026-02-01 23:15:43 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,11 +16,10 @@ const App = () => {
       <store.Provider value={[token, setToken]}>
         <Nav />
         <Routes >
-          <Route path="/Mern-Stack-Project/" element={<Login />} />
-<Route path="/Mern-Stack-Project/register" element={<Register />} />
-<Route path="/Mern-Stack-Project/login" element={<Login />} />
-<Route path="/Mern-Stack-Project/myprofile" element={<Myprofile />} />
-
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/myprofile" element={<Myprofile />} />
         </Routes>
       </store.Provider>
     </div>
