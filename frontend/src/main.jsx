8f13ee5f29150e654a017a898b952a6d1456b390
Koyyada-Akhil <akhilakkhi6@gmail.com>
2026-02-01 23:47:54 +0530

    updtaed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,8 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-
-const basename = process.env.NODE_ENV === "production"
-  ? "/Mern-Stack-Project"
-  : "/";
-
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={basename}>
+  <BrowserRouter  >
     <App />
   </BrowserRouter>
 );
